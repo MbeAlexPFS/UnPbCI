770269c991f9c64f1e9be95ccb7c8a88cca66632
Mbe Alexandre <alexmbe8921@gmail.com>
2026-09-18 20:12:15 +0000

    link fix

--- script/app.js
+++ script/app.js
@@ -1,13 +1,14 @@
 //------ Données ------
-sessionStorage.setItem("city","Abidjan")
+sessionStorage.setItem("city", "Abidjan")
 //site donnée
 let sitedata
-fetch('../data/data.json')
-        .then(response => response.json())
-        .then(data => {
-            sitedata = data
-        }).catch(error => { console.error('Error loading JSON data:', error);
-        });
+fetch('data/data.json')
+    .then(response => response.json())
+    .then(data => {
+        sitedata = data
+    }).catch(error => {
+        console.error('Error loading JSON data:', error);
+    });
 
 //------ Rendre la carte ------
 //Généré des couleurs
@@ -28,14 +29,14 @@ var width = window.innerWidth,
 var svg = d3.select("#my_dataviz")
     .attr("width", width)
     .attr("height", height)
-    .on("click",reset)
+    .on("click", reset)
 
 // map et projection
 var projection
 var path
 
 // charge la carte
-d3.json("../data/gadm41_CIV_3.json").then(function (data) {
+d3.json("data/gadm41_CIV_3.json").then(function (data) {
     // Ajuster la projection
     projection = d3.geoMercator().fitSize([width, height], data);
     path = d3.geoPath().projection(projection);
@@ -62,16 +63,16 @@ d3.json("../data/gadm41_CIV_3.json").then(function (data) {
         .attr("text-anchor", "middle")
         .attr("x", d => path.centroid(d)[0])
         .attr("y", d => path.centroid(d)[1])
-        .attr("onclick",d => "toPage('"+d.properties.NAME_3+"')").text(d => d.properties.NAME_3)
+        .attr("onclick", d => "toPage('" + d.properties.NAME_3 + "')").text(d => d.properties.NAME_3)
 
-        //première mise à jours
-        init()
+    //première mise à jours
+    init()
 });
 
 //-- Fonction de zoom --
 //Zoom sur une region
 function zoomToFeature(event, d) {
-    if (! event.target.classList.contains("no-checked")) { //ne pas zoomer lors de la deselection
+    if (!event.target.classList.contains("no-checked")) { //ne pas zoomer lors de la deselection
         // Empêche le reset aussi
         event.stopPropagation();
 
@@ -113,25 +114,25 @@ function init() {
     selectedRegion = "empty"
     searchInput = document.querySelector("#search")
     searchInput.oninput = () => {
-        update()    
+        update()
     }
 
     //selectionne et deselectionne une region
     let mp = document.querySelectorAll(".MultiPolygon")
     mp.forEach((p) => {
-        p.setAttribute("stroke",getRandomColor())
+        p.setAttribute("stroke", getRandomColor())
         p.onclick = () => {
             if (p.classList.contains("no-checked")) {
                 mp.forEach((e) => {
-                    if (! e.classList.contains("no-checked") ) {
+                    if (!e.classList.contains("no-checked")) {
                         e.classList.add("no-checked")
                     }
                 })
                 p.classList.remove("no-checked")
                 p.setAttribute("fill", p.getAttribute("stroke"))
                 selectedRegion = p.getAttribute("name")
-                update() 
-            }else{
+                update()
+            } else {
                 p.classList.add("no-checked")
                 selectedRegion = "empty"
                 update()
@@ -144,7 +145,7 @@ function init() {
 }
 
 function toPage(city) {
-    sessionStorage.setItem("city",city)
+    sessionStorage.setItem("city", city)
     location.replace("page/site.html")
 }
 
@@ -153,15 +154,15 @@ function update() {
     let container = document.querySelector("#container")
     container.innerHTML = ``
     Object.keys(sitedata).forEach((region) => {
-    if (Object.keys(sitedata[selectedRegion]).length == 0 && selectedRegion != "empty") { //aucun site dans un region
-        container.innerHTML = `<div title="empty" class="w-100 h-100 bg-secondary text-white d-flex align-items-center text-center p-2"><h6>Aucun site trouvé dans cette la region ${selectedRegion}</h6></div>`      
-        
-    }else if (Object.keys(sitedata[region]).length > 0) { 
-        Object.keys(sitedata[region]).forEach((el) => {
-            let site = sitedata[region][el]
-            if (region == selectedRegion && (site.Title).toLowerCase().includes(searchInput.value.toLowerCase()) || ((site.Title).toLowerCase().includes(searchInput.value.toLowerCase()) && selectedRegion == "empty" )) {
-                container.innerHTML +=
-                `<div class="shadow p-3 mb-5 rounded border border-dark">
+        if (Object.keys(sitedata[selectedRegion]).length == 0 && selectedRegion != "empty") { //aucun site dans un region
+            container.innerHTML = `<div title="empty" class="w-100 h-100 bg-secondary text-white d-flex align-items-center text-center p-2"><h6>Aucun site trouvé dans cette la region ${selectedRegion}</h6></div>`
+
+        } else if (Object.keys(sitedata[region]).length > 0) {
+            Object.keys(sitedata[region]).forEach((el) => {
+                let site = sitedata[region][el]
+                if (region == selectedRegion && (site.Title).toLowerCase().includes(searchInput.value.toLowerCase()) || ((site.Title).toLowerCase().includes(searchInput.value.toLowerCase()) && selectedRegion == "empty")) {
+                    container.innerHTML +=
+                        `<div class="shadow p-3 mb-5 rounded border border-dark">
                 
                     <div class="card-title">
                         <h4 >${site.Title}</h4>
@@ -172,7 +173,7 @@ function update() {
                     <div class="d-grid gap-2">
                         <button
                             type="button"
-                            name="`+region+`,`+el+`"
+                            name="`+ region + `,` + el + `"
                             data-bs-toggle="modal" data-bs-target="#seeMore"
                             class="btn btn-primary showBtn"
                         >
@@ -181,28 +182,29 @@ function update() {
                     </div>
                 
                 </div> `
-            }    
-        })
-        
-    }})
+                }
+            })
+
+        }
+    })
     if (searchInput.value != "" && container.innerHTML == ``) {
-        container.innerHTML = `<div title="empty" class="w-100 h-100 bg-secondary text-white d-flex align-items-center text-center p-2"><h6>Aucun site ne correspond à votre recherche</h6></div>`   
+        container.innerHTML = `<div title="empty" class="w-100 h-100 bg-secondary text-white d-flex align-items-center text-center p-2"><h6>Aucun site ne correspond à votre recherche</h6></div>`
     }
     document.querySelectorAll(".showBtn").forEach((btn) => {
-        btn.onclick = () => { show(btn.getAttribute("name").split(",")[0],btn.getAttribute("name").split(",")[1]) }
-    })  
-     
+        btn.onclick = () => { show(btn.getAttribute("name").split(",")[0], btn.getAttribute("name").split(",")[1]) }
+    })
+
 }
 
 //change popup data
 
 
-function show(region,site) {
+function show(region, site) {
     let sregion = document.querySelector("#showRegion")
     sregion.textContent = region
     let popup = document.querySelector("#show")
     popup.innerHTML =
-    `<div class="text-end">
+        `<div class="text-end">
             <h1>${sitedata[region][site].Title}</h1>
             <p>${sitedata[region][site].ShortDesc}</p>
         </div>
@@ -210,7 +212,7 @@ function show(region,site) {
         ${sitedata[region][site].Desc}
     </p>
     <p class="text-center mt-2">
-        <button class="btn btn-primary" onclick="toPage('${ region }')">Site de la ville</button>
+        <button class="btn btn-primary" onclick="toPage('${region}')">Site de la ville</button>
     </p>
     `
 }
